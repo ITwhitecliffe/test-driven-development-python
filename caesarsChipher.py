1133f536fa0b2c683e8e094083c4bc64ec0e8267
Ying Chiu Goh <YingG@whiteclife.ac.nz>
2023-06-21 11:10:23 +1200

    testing demo

---
+++ caesarsChipher.py
@@ -0,0 +1,14 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+def encrypt(text, shift):
+    userInput = text + shift
+    return userInput
+
+if __name__ == '__main__':
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    print(encrypt())
---
+++ first_module.py
@@ -0,0 +1,26 @@
+# print(__name__)
+
+# def enterNumber():
+#     num = int(input('What is the number?'))
+
+#     if num == 0:
+#         return 'Zero'
+#     else:
+#         return num
+
+# if __name__ == '__main__':
+#     result = enterNumber()
+#     print(result)
+    
+# if __name__ == '__main__':
+#     print( "Run Directly")
+# else:
+#     print("Run from Import")
+
+# print("This will always be run")
+
+def main():
+    print("First Module's Main Method")
+    
+if __name__ == '__main__':
+    main()
